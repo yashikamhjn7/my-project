01fa54ebd787f489829a2356b5607a56fa073498
yashikamhjn7 <yashikamahajan2004@gmail.com>
2025-05-28 05:38:45 +0000

    second commit

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Starting Spring Boot Application...");
+		System.out.println("Heyy guys.");
 		SpringApplication.run(Main.class, args);
 	}
 
